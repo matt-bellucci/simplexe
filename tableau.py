ea52b91c74425a6b20c5ffe2669b14b6434e4db2
Matthieu Bellucci <matt7469@gmail.com>
2019-04-11 15:23:13 +0200

    Ajout de classe tableau

---
+++ tableau.py
@@ -0,0 +1,36 @@
+import numpy as np
+class Tableau:
+
+	def __init__(self,A):
+		self.tab = A
+		self.nb_lines = A.shape[0]
+		self.nb_cols = A.shape[1]
+
+	def add_lines(self,i,j):
+		"""
+		Additionne la ligne j à la ligne i
+
+		"""
+		for k in range(self.nb_cols):
+			self.tab[i][k] += self.tab[j][k]
+
+	def mult_lines(self,i,coef):
+
+		for k in range(self.nb_cols):
+			self.tab[i] *= coef
+
+	def __str__(self):
+		return np.array_str(self.tab)
+
+
+
+def main():
+	A = np.array([[3,4,5],[6,7,8]])
+	tab = Tableau(A)
+	print(tab)
+	tab.add_lines(0,1)
+	print(tab)
+	tab.mult_lines(0,-1)
+	print(tab)
+if __name__ == "__main__":
+	main()
